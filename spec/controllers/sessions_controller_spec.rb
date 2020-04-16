c631f56c80b22008f3b49e066bdf48785b65e8c1
Rory Hellier <rory.hellier@googlemail.com>
2020-04-16 18:27:49 +0100

    Filled out Sessions Controller Tests and Finished off Sessions Helper Tests
     - Added let statement for creating the user in user helper, session helper and session controller statements
     - Added test in sessions helper tests to test log out method
     - Added two tests to the session controller tests (failing on purpose)

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,8 +1,15 @@
-RSpec.describe User, type: :controller do
-  it 'creates new session for the user' do
-    user = User.create!(name: 'Test')
+require 'rails_helper'
+include SessionsHelper
+
+RSpec.describe SessionsController, type: :controller do
+  let(:user) { User.create!(name: 'Test') }
+  
+  it 'should log the user out' do
     log_in user
-  end
-    
+    delete :destroy
+    expect(@current_user).to be_nil
+    expect(session[:name]).to be_nil
+    expect(response).to redirect_to(root_path)
+    expect(flash[:success]).to be_present
   end
 end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,33 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
+  let(:user) { User.create!(name: 'Test')}
+
   it 'stores the users name in the session cookie' do
-    user = User.create!(name: 'Test')
     log_in user
     expect(session[:name]).to eq("#{user.name}")
   end
 
   it 'sets the current user to the user' do
-    user = User.create!(name: 'Test')
     log_in user
     expect(@current_user).to eq(user)
   end
 
   it 'checks if the current user exists' do
-    user = User.create!(name: 'Test')
     log_in user
     expect(@current_user).to eq(user)
   end
 
   it 'checks if the current user in not nil' do
-    user = User.create!(name: 'Test')
     log_in user
     expect(logged_in?).to be_truthy
   end
 
   context 'logs out user' do
     it 'deletes the session' do
-      user = User.create!(name: 'Test')
       log_in user
       log_out user
       expect(session[:name]).to be_nil
