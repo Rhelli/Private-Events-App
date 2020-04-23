3fe961bf797e650f28948b78941470446f87bc50
Rory Hellier <rory.hellier@googlemail.com>
2020-04-23 16:28:56 +0100

    Continued Styling The Home Page
     - Began Building the sitewide navbar in a new partial
     - Added background texture for sitewide background
     - Added home card white background for all pages
     - Added font-awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 
 import './bootstrap_custom.js'
 import '../../assets/stylesheets/application.css'
+import '@fortawesome/fontawesome-free/js/all'
+require.context('../../assets/images', true)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
