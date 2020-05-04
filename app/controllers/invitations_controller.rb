b69023fb047fb04b6f60a09a0f9b9ce0721bf3c5
Rory Hellier <rory.hellier@googlemail.com>
2020-05-04 18:48:06 +0100

    Filled out Invitations Controller
     - Added new method for invitations
     - Added create method for invitations with flash messages
     - Added show method with g variable of @event, pulling from invited_event from invitations
     - Added update method to update the rsvp column in invitations as users accept or decline event invitations
     - Added destroy method for invitations
     - Added strong params for invitations

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,43 @@
+class InvitationsController < ApplicationController
+
+  def new
+    @invitation = Invitation.new
+  end
+
+  def create
+    @invitation = Invitation.new(invitation_params)
+    if @invitation.save
+      flash[:success] = "Your invitation has been sent."
+      redirect_to event_path(@invitation.invited_to_event)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @event = @invitation.invited_to_event
+  end
+
+  def update
+    if rsvp_params == 'true'
+      @invitation.accept
+      flash[:info] = "Invitation accepted! This event has been added to your 'Upcoming Events' list."
+    else
+      @invitiation.decline
+      flash[:info] = "You have declined to attend this event. #{view_context.link_to("Go Back", @invitation)}".html_safe
+      redirect_to invitation_path(@invitation)
+  end
+
+  def destroy
+    @invitation.destroy
+    flash[:info] = "Invitation successfully deleted."
+    redirect_to root_url
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:event_attendee_id, :invited_to_event_id)
+  end
+
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   has_many :invitations, foreign_key: :invited_to_event_id
   has_many :attendees, through: :invitations, source: :event_attendee
 
-  def attendees
-    self.attendees.where('rsvp = ?', true)
-  end
+  #def attendees
+  #  self.attendees.where('rsvp = ?', true)
+  #end
 end
