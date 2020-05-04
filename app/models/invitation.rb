42df9f9d03192513c755af70e1ec59a5fd7994ed
Rory Hellier <rory.hellier@googlemail.com>
2020-05-04 18:16:52 +0100

    Added Attendees method to event controller
     - Added atendees method to event controller which defines whether users are displayed as attendees to an event based on the RSVP boolean
     - Added the invitation show page
     - Invitations model and views needs to be filled out

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,12 @@
 class Invitation < ApplicationRecord
   belongs_to :invited_to_event, class_name: 'Event'
   belongs_to :event_attendee, class_name: 'User'
+
+  def accepted
+    self.update(rsvp: true)
+  end
+
+  def declined
+    self.update(rsvp: false)
+  end
 end
