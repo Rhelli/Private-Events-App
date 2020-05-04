e43e656ae5a9a1b6212dfc33c2211eee4e6dcd0c
Rory Hellier <rory.hellier@googlemail.com>
2020-05-04 16:54:09 +0100

    Added datetime slect to the event creation card

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
   has_many :invitations, foreign_key: :invited_to_event_id
   has_many :attendees, through: :invitations, source: :event_attendee
 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   
   resources :events
   resources :users
+  resources :invitations
 end
