a633a29394946a8adf6cba42dd837a013bbf8c62
Rory Hellier <rory.hellier@googlemail.com>
2020-05-05 14:20:23 +0100

    Created Invitation Show Page
     - Styled and added invitation card
     - New font for title and bolierplate text
     - Boilerplate text is not fully routed in yet.
     - Checkbox added in and styled
     - Everything needs routing up

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,5 @@
 class InvitationsController < ApplicationController
+  before_action :fetch_invite, only: [:show, :update, :destroy]
 
   def new
     @invitation = Invitation.new
@@ -26,6 +27,7 @@ class InvitationsController < ApplicationController
       @invitiation.decline
       flash[:info] = "You have declined to attend this event. #{view_context.link_to("Go Back", @invitation)}".html_safe
       redirect_to invitation_path(@invitation)
+    end
   end
 
   def destroy
@@ -42,6 +44,10 @@ class InvitationsController < ApplicationController
 
   def rsvp_params
     params.require(:response) 
+  end
+
+  def fetch_invite
+    @invitation = Invitation.find_by(id: params[:id])
   end
 
 end
