4adf4914f5e2087184cdf5d36b651710c2c499d9
Rory Hellier <rory.hellier@googlemail.com>
2020-05-11 20:45:10 +0100

    Added Invitation Model Association tests
     - Added validation test for the the invited_to_event association
     - Added validation test for the event_attendee association

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,2 +1,19 @@
 require 'rails_helper'
 
+RSpec.describe Invitation, type: :model do
+  context 'Invitation model associations' do
+    it "belongs to an invited_to_event" do
+      x = Invitation.reflect_on_association(:invited_to_event)
+      expect(x.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to the event_attendee' do
+      x = Invitation.reflect_on_association(:event_attendee)
+      expect(x.macro).to eq(:belongs_to)
+    end
+  end
+
+  context 'tests for methods defining if the invite is accepted or declined' do
+
+  end
+end
