e3a7de947e7229ce25d98f48147b104ef2bdcb32
Rory Hellier <rory.hellier@googlemail.com>
2020-05-11 19:52:40 +0100

    Added Event Model Validations And Began Testing
     - Added event model validations for name (presence, length and not allowing nil)
     - Downcased and capitalized names before saving
     - Added validation for event date (presence only)
    
     - Added validation for description (presence, max length of 20,000 chars)
     - Added validation for presence of creator ID
     - Added test in event_spec for proper config
     - Added test in event_spec for rejecting new event with nil name

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,2 +1,18 @@
 require 'rails_helper'
 
+RSpec.describe Event, type: :model do
+  let!(:user) { User.create!(id: '1', name: 'Test') }
+  context 'Event model validations' do
+    it "it validates correct event creation config" do
+      user
+      event = Event.create!(name: 'Test', event_date: '2020-06-08 22:00:00 UTC', creator_id: '1', description: 'magnets')
+      expect(event).to be_truthy
+    end
+
+    it "rejects an event without a given name" do
+      user
+      event = Event.new(name: ' ', event_date: '2020-06-08 22:00:00 UTC', creator_id: '1', description: 'magnets')
+      expect(event).to_not be_valid
+    end
+  end
+end
