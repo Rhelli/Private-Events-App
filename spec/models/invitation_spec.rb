904ee9c7077ced941e1751da72e25963098b900a
Rory Hellier <rory.hellier@googlemail.com>
2020-05-12 12:26:09 +0100

    Invitation model testing completed
     - Testing for invitation associations (invited_to_event_ids/event_attendee_ids) completed and passing
     - Test to reject duplicate invites to the same attendee created and passing
     - Tests for the accepted/declined rsvp column update method created and passing
     - Added lets for various example users, events and invitations

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
+  let(:user1) { User.create!(name: 'Test') }
+  let(:user2) { User.create!(name: 'Test2') }
+  let(:example_event) {  Event.create!(name: 'Boogietime', event_date: '2019-11-11 21:00:00 UTC', created_at: '2019-11-10 19:00:00 UTC', updated_at: '2019-11-10 19:00:00 UTC', creator_id: '1', description: 'Ghouls') }
+  let(:example_invite) { Invitation.create!(event_attendee_id: '2', invited_to_event_id: '1', rsvp: nil, created_at: '2019-10-10 20:00:00 UTC', updated_at: '10-10-2019 20:00:00 UTC') }
   context 'Invitation model associations' do
     it "belongs to an invited_to_event" do
       x = Invitation.reflect_on_association(:invited_to_event)
@@ -13,7 +17,34 @@ RSpec.describe Invitation, type: :model do
     end
   end
 
-  context 'tests for methods defining if the invite is accepted or declined' do
+  context 'it validates invitation creation' do
+    it 'rejects duplicate invites to one user' do
+      user1
+      user2
+      example_event
+      example_invite
+      invite = Invitation.new(event_attendee_id: '2', invited_to_event_id: '1', rsvp: 'false', created_at: '2019-10-10 20:00:00 UTC', updated_at: '10-10-2019 20:00:00 UTC')
+      expect(invite).to_not be_valid
+    end
+  end
 
+  context 'tests for methods defining if the invite is accepted or declined' do
+    it 'updates the invitations rsvp column to true when accepted called' do
+      user1
+      user2
+      example_event
+      example_invite
+      example_invite.accepted
+      expect(example_invite.rsvp).to eq(true)
+    end
+
+    it "updates the invitation's rsvp column to false when the declined method is called" do
+      user1
+      user2
+      example_event
+      example_invite
+      example_invite.declined
+      expect(example_invite.rsvp).to eq(false)
+    end
   end
 end
