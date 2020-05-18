0c233d350d4b2d9db8d139cd4c0d3584239fa88d
Rory Hellier <rory.hellier@googlemail.com>
2020-05-18 18:14:05 +0100

    Finished Editing the User Invitation Page
     - Added the date for the event in a pleasant format
     - Changed the color of the font for style

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,6 +3,10 @@ class Invitation < ApplicationRecord
   belongs_to :event_attendee, class_name: 'User'
   validates_uniqueness_of :event_attendee, scope: 'invited_to_event_id', message: 'This member is already invited.'
 
+  def date
+    invited_to_event.event_date.strftime('%I:%M %P, %B %-d, %Y')
+  end
+
   def accepted
     update(rsvp: true)
   end
