c67944a4df19d00e0ebbac9147a09af7dd7dd42e
Rory Hellier <rory.hellier@googlemail.com>
2020-05-20 10:58:22 +0100

    Added validation on events so events cannot be created in a past date

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,6 +2,7 @@ class Invitation < ApplicationRecord
   belongs_to :invited_to_event, class_name: 'Event'
   belongs_to :event_attendee, class_name: 'User'
   validates_uniqueness_of :event_attendee, scope: 'invited_to_event_id', message: 'This member is already invited.'
+  validate :upcoming_event
 
   def date
     invited_to_event.event_date.strftime('%I:%M %P, %B %-d, %Y')
